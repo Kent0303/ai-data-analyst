21eed1cd117de1aaddd19ae8cf39ac8c53f6003c
Kent <128348255+Kent0303@users.noreply.github.com>
2026-03-12 16:46:35 +0800

    Fix: Use standalone output for API routes support

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
-/** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig } */
 const nextConfig = {
-  output: 'export',
-  distDir: 'dist',
+  output: 'standalone',
+  di}Äir: '.next',
   images: {
     unoptimized: true
   }
